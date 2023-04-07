cf8f7dd5d357aee987d4d25f02c5b00a48496c20
Arvind_Cardr <arvind@cardr.com>
2023-04-07 17:19:48 +0530

    library

---
+++ cardrconnection/src/main/java/com/obd/cardrconnection/OBDManager.java
@@ -0,0 +1,12 @@
+package com.obd.cardrconnection;
+
+import voyomotive.voyolibrary.VoyoAPI;
+
+public class OBDManager {
+VoyoAPI voyoAPI;
+
+    public  static void createLoginRequest(){
+
+
+    }
+}
